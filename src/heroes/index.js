52bccbc4c9e83952d28fae9a0448f9bf1f7549dd
Mona Lisa <santifranquez@gmail.com>
2022-09-09 21:58:57 -0300

    Creando nuevas rutas y configurando

--- src/heroes/index.js
+++ src/heroes/index.js
@@ -1,1 +1,4 @@
 export * from './pages';
+
+// Cómo es uno solo se hace de esta forma, sino se haría como todos los restos de archivos de barril
+export * from './routes/HeroesRoutes'
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -33,6 +33,15 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+
+                    <NavLink 
+                        className={({isActive}) => {
+                            return `nav-item nav-link ${(isActive) ? 'active' : null}`
+                        }}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
